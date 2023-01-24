e81ce5ff8389bc57970ce90a628a5dc2c7576088
anji0114 <anji.tnk.140114@gmail.com>
2023-01-24 22:32:06 +0900

    お問い合わせの動線の導入

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,28 @@
 import { FooterContact } from "@/components/Footer/Contact";
 import { Logo } from "@/components/Logo";
 import { SnsLinks } from "@/components/SnsLinks";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
     <footer className="l-footer">
       <FooterContact />
       <div className="l-container">
-        <div className="l-footer__contents">
-          <div className="l-footer__logo">
-            <Logo />
+        <div className="l-footer__inner">
+          <div className="l-footer__contents">
+            <div className="l-footer__logo">
+              <Logo />
+            </div>
+            <div className="l-footer__links">
+              <SnsLinks />
+            </div>
           </div>
-          <div className="l-footer__links">
-            <SnsLinks />
-            <p className="l-footer__privacy">©︎2023 Anji Tanaka</p>
-          </div>
+          <p className="l-footer__bottom">
+            <span className="l-footer__privacy">©︎2023 Anji Tanaka</span>
+            <Link href="mailto:tanaka.anji14@gmail.com" className="l-footer__mail">
+              Mail: tanaka.anji14@gmail.com
+            </Link>
+          </p>
         </div>
       </div>
     </footer>
