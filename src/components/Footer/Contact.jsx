a16843b506bc7106a092d781051706ebd98eee71
anji0114 <anji.tnk.140114@gmail.com>
2023-01-27 09:58:34 +0900

    テキストの修正

--- src/components/Footer/Contact.jsx
+++ src/components/Footer/Contact.jsx
@@ -5,8 +5,8 @@ export const FooterContact = () => {
     <div className="p-contact">
       <Link href={"mailto:tanaka.anji14@gmail.com"} className="p-contact__link">
         <p className="p-contact__text">
-          <span className="c-gradient__text-wrap" data-text="Contact Us">
-            <span className="c-gradient__text">Contact Us</span>
+          <span className="c-gradient__text-wrap" data-text="Contact">
+            <span className="c-gradient__text">Contact</span>
           </span>
           <span className="p-contact__arrow c-gradient__arrow"></span>
         </p>
