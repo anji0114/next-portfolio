23988ec8a03082531c1f6ca012c8b0b46a30c5e0
anji0114 <anji.tnk.140114@gmail.com>
2023-05-15 08:56:51 +0900

    mvの修正

--- src/components/Home/HomeMv.tsx
+++ src/components/Home/HomeMv.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import lightLogo from "src/images/icons/logo_mv-light.svg";
 import darkLogo from "src/images/icons/logo_mv-dark.svg";
 import githubIcon from "src/images/icons/icon_github.svg";
+import zennIcon from "src/images/icons/icon_zenn.svg";
+import gatherIcon from "src/images/icons/icon_gather.svg";
 import Link from "next/link";
 
 export const HomeMv = (): JSX.Element => {
@@ -23,14 +25,15 @@ export const HomeMv = (): JSX.Element => {
                 PortFolio
               </span>
             </p>
+
             <p className="p-home-mv__text">
               <Link
                 href="https://github.com/anji0114/next-portfolio"
-                className="p-home-mv__text-link"
+                className="p-home-mv__link"
                 target="_blank"
               >
                 <Image src={githubIcon} alt="" width={30} height={30} />
-                使用技術
+                ポートフォリオ使用技術
               </Link>
               <br />
               <span
@@ -40,6 +43,25 @@ export const HomeMv = (): JSX.Element => {
                 <span className="c-gradient__text">React.js / Next.js / Typescript / microCMS</span>
               </span>
             </p>
+            <div className="p-home-mv__links">
+              <Link
+                href="https://zenn.dev/anji_dev/books/c57b80fc8fc403"
+                className="p-home-mv__link p-home-mv__links-item"
+                target="_blank"
+              >
+                <Image src={zennIcon} alt="" width={30} height={30} />
+                Zenn Books
+              </Link>
+
+              <Link
+                href="https://gathernote.vercel.app/"
+                className="p-home-mv__link p-home-mv__links-item"
+                target="_blank"
+              >
+                <Image src={gatherIcon} alt="" width={30} height={30} />
+                個人開発アプリ
+              </Link>
+            </div>
           </div>
         </div>
         {/* image */}
